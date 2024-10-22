90fbc3dee2e4cc35ef335b725b293a5e8f69682e
Sefa Erdoğdugil <s.erdogdugil@gmail.com>
2024-10-22 16:53:51 +0300

    Update

--- odev2Py.py
+++ odev2Py.py
@@ -4,12 +4,10 @@ def tek_cift_bulma(sayi):
     else:
         print(f"{sayi} bir tek sayıdır.")
 
-
 def sayilari_siralama(sayi1, sayi2, sayi3):
     sayilar = [sayi1, sayi2, sayi3]
     sayilar.sort(reverse=True)
-    print("Sıralanmış sayılar: ", sayilar)
-
+    print("Büyükten küçüğe sıralanmış sayılar: ", sayilar)
 
 def asal_sayi_bulma(sayi):
     if sayi < 2:
@@ -21,35 +19,31 @@ def asal_sayi_bulma(sayi):
             return
     print(f"{sayi} bir asal sayıdır.")
 
+while True:
+    print("\n(1) Girilen sayının Tek Yada Çift Olduğunu Bulma")
+    print("(2) Girilen 3 sayıyı büyükten küçüğe sıralama")
+    print("(3) Girilen Sayının Asal sayı olup olmadığını bulma")
+    print("Çıkmak için -1'e basınız.")
 
-def main():
-    while True:
-        print("\n(1) Girilen sayının Tek Yada Çift Olduğunu Bulma")
-        print("(2) Girilen 3 sayıyı büyükten küçüğe sıralama")
-        print("(3) Girilen Sayının Asal sayı olup olmadığını bulma")
+    secim = int(input("Seçiminizi Giriniz (1/2/3): "))
 
-        secim = int(input("Seçiminizi Giriniz (1/2/3): "))
-
-        if secim == 1:
-            sayi = int(input("Bir sayı giriniz: "))
-            tek_cift_bulma(sayi)
-
-        elif secim == 2:
-            sayi1 = int(input("1. sayıyı giriniz: "))
-            sayi2 = int(input("2. sayıyı giriniz: "))
-            sayi3 = int(input("3. sayıyı giriniz: "))
-            sayilari_siralama(sayi1, sayi2, sayi3)
-
-        elif secim == 3:
-            sayi = int(input("Bir sayı giriniz: "))
-            asal_sayi_bulma(sayi)
-
-        else:
-            print("Yanlış değer girdiniz tekrar giriş yapınız.")
-            continue
-
+    if secim == -1:
+        print("Çıkılıyor...")
         break
 
+    elif secim == 1:
+        sayi = int(input("Bir sayı giriniz: "))
+        tek_cift_bulma(sayi)
 
-if __name__ == "__main__":
-    main()
+    elif secim == 2:
+        sayi1 = int(input("1. sayıyı giriniz: "))
+        sayi2 = int(input("2. sayıyı giriniz: "))
+        sayi3 = int(input("3. sayıyı giriniz: "))
+        sayilari_siralama(sayi1, sayi2, sayi3)
+
+    elif secim == 3:
+        sayi = int(input("Bir sayı giriniz: "))
+        asal_sayi_bulma(sayi)
+
+    else:
+        print("Yanlış değer girdiniz, lütfen tekrar deneyiniz.")
